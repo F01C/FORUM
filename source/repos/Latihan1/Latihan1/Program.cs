3008b7826e737aafb8ec0786bd0e9aa9b110da74
Fadhil Dzaky Arhab <144238664+AleisterAtia@users.noreply.github.com>
2024-09-18 14:41:22 +0700

    Baruu

---
+++ source/repos/Latihan1/Latihan1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Latihan1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Input data pribadi
+            Console.Write("Masukkan Nama: ");
+            string nama = Console.ReadLine();
+
+            Console.Write("Masukkan Nomor BP: ");
+            string nobp = Console.ReadLine();
+
+            Console.Write("Masukkan Kelas: ");
+            string kelas = Console.ReadLine();
+
+            Console.Write("Masukkan Mata Kuliah: ");
+            string mataKuliah = Console.ReadLine();
+
+            // Input nilai UTS dan UAS
+            Console.Write("Masukkan Nilai UTS: ");
+            double nilaiUTS = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Masukkan Nilai UAS: ");
+            double nilaiUAS = Convert.ToDouble(Console.ReadLine());
+
+            // Menghitung nilai akhir (40% UTS + 60% UAS)
+            double nilaiAkhir = (0.4 * nilaiUTS) + (0.6 * nilaiUAS);
+
+            // Menampilkan hasil
+            Console.WriteLine("\n--- Hasil ---");
+            Console.WriteLine($"Nama: {nama}");
+            Console.WriteLine($"Nomor BP: {nobp}");
+            Console.WriteLine($"Kelas: {kelas}");
+            Console.WriteLine($"Mata Kuliah: {mataKuliah}");
+            Console.WriteLine($"Nilai Akhir: {nilaiAkhir}");
+
+            
+        }
+    }
+}
---
+++ source/repos/MI2B(Project)/MI2B(Project)/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MI2B_Project_
+{
+    public class Mahasiswa
+    {
+
+        private string nim;
+        private string nama;
+        
+
+        public string Nim
+        {
+            get { return nim; }
+            set { nim = value; }
+        }
+
+        public string Nama
+        {
+            get { return nama; }
+            set { nama = value; }
+        }
+        
+    }
+
+    public enum Hari { Senin, Selasa, Rabu, Kamis, Jumat, Sabtu, Minggu }
+
+    class Program
+    {
+        static void Main(string[] arg)
+        {
+            int a = 5;
+            int b = a;
+            a = 12;
+
+           // Console.WriteLine($"A : {a} ");
+            //Console.WriteLine($"B : {b} ");
+            //Console.ReadLine();
+
+
+            Mahasiswa mhs1 = new Mahasiswa();
+            mhs1.Nim = "2301092010";
+            mhs1.Nama = "Fadhil Dzaky Arhab";          
+
+            Mahasiswa mhs2 = mhs1;
+
+            Console.WriteLine($"Mahasiswa1 - Nim : {mhs1.Nim}, Nama : { mhs1.Nama} ");
+            Console.WriteLine($"Mahasiswa2 - Nim : {mhs2.Nim}, Nama : { mhs2.Nama} ");
+            
+            mhs1.Nama = "Ambatron";         
+
+
+            Console.WriteLine($"Mahasiswa1 - Nim : {mhs1.Nim}, Nama : { mhs1.Nama} ");
+            Console.WriteLine($"Mahasiswa2 - Nim : {mhs2.Nim}, Nama : { mhs2.Nama} ");
+            
+
+            var nama = "Fadhil Arhab";
+            var bil1 = 10;
+            var bil2 = 12.5;
+            var isSelected = true;
+
+            Console.WriteLine(nama.GetType().ToString());
+            Console.WriteLine(bil1.GetType().ToString());
+            Console.WriteLine(bil2.GetType().ToString());
+            Console.WriteLine(isSelected.GetType().ToString());
+            
+
+            Hari testHari = Hari.Senin;
+            switch (testHari)
+            {
+                case Hari.Senin :
+                Console.WriteLine("Hari Senin");
+                    
+                    break;
+                
+
+                case Hari.Selasa:
+                    Console.WriteLine("Hari Selasa");
+                    
+                    break;
+
+                case Hari.Rabu:
+                    Console.WriteLine("Hari Rabu");
+                    
+                    break;
+
+                case Hari.Kamis:
+                    Console.WriteLine("Hari Kamis");
+                    
+                    break;
+
+                case Hari.Jumat:
+                    Console.WriteLine("Hari Jum'at ");
+                    
+                    break;
+
+                case Hari.Sabtu:
+                    Console.WriteLine("Hari Sabtu");
+                    
+                    break;
+
+                case Hari.Minggu:
+                    Console.WriteLine("Hari Minggu");
+                    
+                    break;
+            }
+
+            /*String Nama="";
+                 string alamat = string.Empty;
+            Console.Write("Masukkan Nama Anda");
+             nama = Console.ReadLine();
+            Console.Write("Masukkan Alamat");
+            alamat = Console.ReadLine();
+             */
+            Console.ReadLine();
+
+
+        }
+    }
+}
